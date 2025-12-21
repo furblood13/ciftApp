02528d0be8683cf94ba667ac490008ee66bdbe13
furblood13 <furkankarci80@outlook.com>
2025-12-21 15:53:34 +0300

    Initial Commit

---
+++ ciftApp/ciftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ciftAppApp.swift
+//  ciftApp
+//
+//  Created by furkan buğra karcı on 21.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ciftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
